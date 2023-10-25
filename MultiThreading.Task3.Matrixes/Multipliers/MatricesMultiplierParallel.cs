c15cf64e2632b2752bab05cab1327997a1484dea
Yulia Antonova <Julia_Antonova1@epam.com>
2023-10-25 17:17:17 +0300

    3. Task3.MatrixMultiplier

--- MultiThreading.Task3.Matrixes/Multipliers/MatricesMultiplierParallel.cs
+++ MultiThreading.Task3.Matrixes/Multipliers/MatricesMultiplierParallel.cs
@@ -1,4 +1,5 @@
 ﻿using MultiThreading.Task3.MatrixMultiplier.Matrices;
+using System.Threading.Tasks;
 
 namespace MultiThreading.Task3.MatrixMultiplier.Multipliers
 {
@@ -6,8 +7,23 @@ namespace MultiThreading.Task3.MatrixMultiplier.Multipliers
     {
         public IMatrix Multiply(IMatrix m1, IMatrix m2)
         {
-            // todo: feel free to add your code here
-            return new Matrix(1, 1);
+
+            var resultMatrix = new Matrix(m1.RowCount, m2.ColCount);
+
+            Parallel.For(0, m1.ColCount, i =>
+            {
+                for (byte j = 0; j < m2.ColCount; j++)
+                {
+                    long sum = 0;
+                    for (byte k = 0; k < m1.ColCount; k++)
+                    {
+                        sum += m1.GetElement(i, k) * m2.GetElement(k, j);
+                    }
+
+                    resultMatrix.SetElement(i, j, sum);
+                }
+            });
+            return resultMatrix;
         }
     }
 }
