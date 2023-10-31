16b23b85cd438c830989e948e8640b1fcccf7579
Yulia Antonova <Julia_Antonova1@epam.com>
2023-10-31 09:45:26 +0200

    task 5 improvements

--- MultiThreading.Task5.Threads.SharedCollection/Program.cs
+++ MultiThreading.Task5.Threads.SharedCollection/Program.cs
@@ -14,6 +14,45 @@ namespace MultiThreading.Task5.Threads.SharedCollection
     {
         private static readonly List<int> sharedCollection = new List<int>();
 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("5. Write a program which creates two threads and a shared collection:");
+            Console.WriteLine("the first one should add 10 elements into the collection and the second should print all elements in the collection after each adding.");
+            Console.WriteLine("Use Thread, ThreadPool or Task classes for thread creation and any kind of synchronization constructions.");
+            Console.WriteLine();
+
+            createThreads();
+
+            Console.ReadLine();
+        }
+
+        private static void createThreads()
+        {
+            Thread addingThread = new Thread(() =>
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    AddElement(i);
+                }
+            });
+
+            Thread printingThread = new Thread(() =>
+            {
+                while (true)
+                {
+                    PrintCollection();
+
+                    if (sharedCollection.Count == 10) break;
+                }
+            });
+
+            addingThread.Start();
+            printingThread.Start();
+
+            addingThread.Join();
+            printingThread.Join();
+        }
+
         private static void AddElement(int element)
         {
             lock (sharedCollection)
@@ -37,41 +76,5 @@ namespace MultiThreading.Task5.Threads.SharedCollection
                 Console.WriteLine();
             }
         }
-        static void Main(string[] args)
-        {
-            Console.WriteLine("5. Write a program which creates two threads and a shared collection:");
-            Console.WriteLine("the first one should add 10 elements into the collection and the second should print all elements in the collection after each adding.");
-            Console.WriteLine("Use Thread, ThreadPool or Task classes for thread creation and any kind of synchronization constructions.");
-            Console.WriteLine();
-            // Create two threads
-            Thread addingThread = new Thread(() =>
-            {
-                for (int i = 1; i <= 10; i++)
-                {
-                    AddElement(i);
-                }
-            });
-
-            Thread printingThread = new Thread(() =>
-            {
-                while (true)
-                {
-                    PrintCollection();
-
-                    // Wait for a short time before printing the collection again
-                    if (sharedCollection.Count == 10) break;
-                }
-            });
-
-            // Start the threads
-            addingThread.Start();
-            printingThread.Start();
-
-            // Wait for the threads to finish
-            addingThread.Join();
-            printingThread.Join();
-            Console.ReadLine();
-        }
-
     }
 }
